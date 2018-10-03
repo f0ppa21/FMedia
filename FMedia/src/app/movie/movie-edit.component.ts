62e43fd5ea435f25d367493dcedaaea6821f786e
Jonas Forsberg <jonas.forsberg@gmail.com>
2018-10-03 10:53:24 +0200

    Edit-sidan första strukturen

--- FMedia/src/app/movie/movie-edit.component.ts
+++ FMedia/src/app/movie/movie-edit.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 
+import { ModalDialogService } from '../core/modal-dialog/modal-dialog.service';
+
 @Component({
   selector: 'app-movie-edit',
   templateUrl: './movie-edit.component.html',
@@ -8,36 +10,38 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class MovieEditComponent implements OnInit {
   id: number;
+  title: string; // TODO: Ändra sedan så att titeln live visar det man skriver i titel-textboxen + om den är tom visas title (ng if)
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private modalDialogService: ModalDialogService) { }
 
   ngOnInit() {
     this.route.parent.params.subscribe((params: Params) => {
       this.id = +params['id'];  // TODO: this.movieService.getMovie(+params['id']).subscribe((movie: IMovie) => { this.movie = movie; });
+      this.title = (this.id === 0 ? 'Lägg till film' : 'Redigera film');
     });
   }
 
   // TODO: Testa om vi måste sätta scrollStrategy som för About för att undvika "hoppig" sida. Sätt helst i ModalDialogService...
   // TODO: Testa även på movies-sidan att öppna en Alert där... ska inte hoppa..
-  // import { ModalDialogService } from '../core/modal-dialog/modal-dialog.service';
-  // constructor(private modalDialogService: ModalDialogService) { }
-  // openConfirm() {
-  //   const self = this;
-  //   this.modalDialogService.confirm({
-  //     width: '400px',
-  //     data: { title: 'Ta bort filmen?',
-  //             message: 'Den kommer inte längre att finnas kvar... nä inte alls, den är helt omöjlig att få tillbaka!' }
-  //   })
-  //     .then(function(result) {
-  //       self.confirmResult = result;
-  //     });
-  // }
 
-  // openAlert() {
-  //   this.modalDialogService.alert({ data: { title: 'Filmen är inte längre tillgänglig!' } })
-  //     .then(function () {
-  //       alert('Nu är vi tillbaka syncront!');
-  //     });
-  // }
+  openConfirm() {
+    const self = this;
+    this.modalDialogService.confirm({
+      width: '400px',
+      data: { title: 'Ta bort filmen?',
+              message: 'Den kommer inte längre att finnas kvar... nä inte alls, den är helt omöjlig att få tillbaka!' }
+    })
+      .then(function(result) {
+        // self.confirmResult = result;
+      });
+  }
 
+  openAlert() {
+    this.modalDialogService.alert({ data: { title: 'Filmen är inte längre tillgänglig!' } })
+      .then(function () {
+        alert('Nu är vi tillbaka syncront!');
+      });
+  }
 }
