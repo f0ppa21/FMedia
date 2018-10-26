bbf6f0b0590341284a4cf8e4e8f4dfd2023db20f
Jonas Forsberg <jonas.forsberg@gmail.com>
2018-10-26 15:46:37 +0200

    Stöd för ReactiveForms

--- FMedia/src/app/movie/movie-edit.component.ts
+++ FMedia/src/app/movie/movie-edit.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router, ActivatedRoute, Params } from '@angular/router';
-import { HostBinding } from '@angular/core';
-import { slideInDownAnimation } from '../shared/router.animations';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
+import { IMovie } from '../shared/interfaces';
+import { slideInDownAnimation } from '../shared/router.animations';
 import { ModalDialogService } from '../core/modal-dialog/modal-dialog.service';
 
 @Component({
@@ -18,10 +19,15 @@ export class MovieEditComponent implements OnInit {
 
   id: number;
   title: string; // TODO: Ändra sedan så att titeln live visar det man skriver i titel-textboxen + om den är tom visas title (ng if)
+  customerForm: FormGroup;
+  movie: IMovie;
+  message: string;
+
 
   constructor(private router: Router,
     private route: ActivatedRoute,
     private location: Location,
+    private formBuilder: FormBuilder,
     private modalDialogService: ModalDialogService) { }
 
   ngOnInit() {
@@ -29,6 +35,19 @@ export class MovieEditComponent implements OnInit {
       this.id = +params['id'];  // TODO: this.movieService.getMovie(this.id).subscribe((movie: IMovie) => { this.movie = movie; });
       this.title = (this.id === 0 ? 'Lägg till film' : 'Redigera film');
     });
+
+    // TODO: Om det är en ny film sätts tomma default-värden, dvs gör ingen subscribe som ovan då...
+    this.movie = {
+      title: 'Mad Max'
+    };
+
+    this.customerForm = this.formBuilder.group({
+      title: [this.movie.title, Validators.required ]
+    });
+  }
+
+  onSubmit() {
+    this.message = 'You typed: ' + this.customerForm.value.title;
   }
 
   cancel(event: Event) {
--- FMedia/src/app/shared/shared.module.ts
+++ FMedia/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule} from '@angular/forms';
 
 import { MaterialModule } from './material.module';
 
 @NgModule({
-  exports: [ CommonModule, MaterialModule, FlexLayoutModule ]
+  exports: [ CommonModule, MaterialModule, FlexLayoutModule, ReactiveFormsModule ]
 })
 export class SharedModule {}  // Exports Shared (multi-instance) objects
