bb5ab3bf7caa1be10374a314269594d29a167de9
Jonas Forsberg <jonas.forsberg@gmail.com>
2019-05-29 14:10:26 +0200

    Material och CDK uppdaterad till Angular 8

--- FMedia/src/app/shared/material.module.ts
+++ FMedia/src/app/shared/material.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core';
 
-import {
-  MatButtonModule,
-  MatMenuModule,
-  MatToolbarModule,
-  MatIconModule,
-  MatCardModule,
-  MatDividerModule,
-  MatDialogModule,
-  MatListModule,
-  MatChipsModule,
-  MatButtonToggleModule,
-  MatInputModule,
-  MatTableModule
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   exports: [
