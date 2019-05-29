6c05215d212a7b35628eae298d4ff389e4d68acc
Jonas Forsberg <jonas.forsberg@gmail.com>
2019-05-29 14:01:10 +0200

    Updated to Angular 8

--- FMedia/src/app/app-routing.module.ts
+++ FMedia/src/app/app-routing.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'movies', loadChildren: './movies/movies.module#MoviesModule' },
-  { path: 'movie/:id', loadChildren: './movie/movie.module#MovieModule' },
+  { path: 'movies', loadChildren: () => import('./movies/movies.module').then(m => m.MoviesModule) },
+  { path: 'movie/:id', loadChildren: () => import('./movie/movie.module').then(m => m.MovieModule) },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', pathMatch: 'full', redirectTo: '/dashboard' } // Catch any unfound routes and redirect to home page
 ];
